8a85e367c129530eeaa2fb6f7b4cd51e307fae26
Camilo Tellez <c.tellezs@uniandes.edu.co>
2024-10-28 17:03:36 -0500

    aca

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { SeriesModule } from './series/series.module';
+import { HttpClientModule } from '@angular/common/http';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SeriesModule,
+    HttpClientModule
   ],
   providers: [
     provideClientHydration()
---
+++ src/app/series/serie.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SerieService {
+
+constructor(private http: HttpClient) { }
+
+}
+
